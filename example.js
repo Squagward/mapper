1eca68df9293aa97096dfcf3dad381d30ca967b3
Cam Walter <cam.n.walter@gmail.com>
2021-08-10 22:49:00 -0500

    Initial commit

---
+++ example.js
@@ -0,0 +1,59 @@
+import { mapField, mapMethod, m, f } from "./index";
+// hover over each for a simple example
+
+// mapField & mapMethod only return the mapped names, while
+// m & f return the value of the field/method applied to the object
+
+const displayWidthName = mapField(Client.getMinecraft(), "displayWidth");
+Client.getMinecraft().field_71443_c;
+
+const actualDisplayWidth = f("displayWidth")(Client.getMinecraft());
+
+console.log(
+  displayWidthName, //                        "field_71443_c"
+  Client.getMinecraft()[displayWidthName], // 1920
+
+  actualDisplayWidth //                       1920
+);
+
+const getSessionMapped = mapMethod(Client.getMinecraft(), "getSession");
+
+const getSessionValue = m("getSession")(Client.getMinecraft());
+
+console.log(
+  getSessionMapped, //                          func_110432_I
+  Client.getMinecraft()[getSessionMapped](), // net.minecraft.util.Session@19c48bb3
+
+  getSessionValue.toString() //                 net.minecraft.util.Session@19c48bb3
+);
+
+///////////////////////////////////////////////////////////////////////
+
+// regular ways
+const player = f("thePlayer")(Client.getMinecraft());
+const name1 = m("getName")(player);
+
+// or via pipeline operator
+const name2 = Client.getMinecraft() |> f("thePlayer") |> m("getName");
+
+console.log(
+  name1, // Squagward
+  name2 //  Squagward
+);
+
+// other examples
+const GlStateManager = Java.type(
+  "net.minecraft.client.renderer.GlStateManager"
+);
+
+register("renderOverlay", () => {
+  GlStateManager |> m("pushMatrix");
+
+  // Note: the m function can take in as many parameters as the method requires
+  GlStateManager |> m("scale", 0.5, 0.5, 1);
+
+  // will draw rectangle with 50 width, 50 height due to the scaling
+  Renderer.drawRect(Renderer.AQUA, 10, 10, 100, 100);
+
+  GlStateManager |> m("popMatrix");
+});
---
+++ index.js
@@ -0,0 +1,60 @@
+/// <reference types="../CTAutocomplete" />
+/// <reference lib="esnext" />
+
+const fields = JSON.parse(FileLib.read("mapper", "fields.json"));
+const methods = JSON.parse(FileLib.read("mapper", "methods.json"));
+
+const finder = (object, name, type) => {
+  const arr = type === "field" ? fields : methods;
+  return arr[name]?.find((v) => !!object[v]);
+};
+
+/**
+ * @example const displayWidth = mapField(Client.getMinecraft(), "displayWidth");
+ * // displayWidth === "field_71443_c"
+ * @returns {string} the mapped field name
+ */
+export const mapField = (object, field) => {
+  const mapped = finder(object, field, "field");
+  if (mapped) return mapped;
+  throw new Error(
+    `No deobfuscated field "${field}" for class ${object.class.getName()} found`
+  );
+};
+
+/**
+ * @example const session = mapMethod(Client.getMinecraft(), "getSession");
+ * // session === "func_110432_I"
+ * @returns {string} the mapped method name
+ */
+export const mapMethod = (object, method) => {
+  const mapped = finder(object, method, "method");
+  if (mapped) return mapped;
+  throw new Error(
+    `No deobfuscated method "${method}" for class ${object.class.getName()} found`
+  );
+};
+
+/**
+ * @example const displayWidth = Client.getMinecraft() |> f("displayWidth");
+ * // displayWidth === 1920
+ * @returns the value of the object's field
+ */
+export const f = (field) => {
+  return (object) => {
+    const mapped = mapField(object, field);
+    return object[mapped];
+  };
+};
+
+/**
+ * @example const session = Client.getMinecraft() |> m("getSession");
+ * // session === net.minecraft.util.Session@19c48bb3
+ * @returns the return value of the object's method
+ */
+export const m = (method, ...params) => {
+  return (object) => {
+    const mapped = mapMethod(object, method);
+    return params?.length ? object[mapped](...params) : object[mapped]();
+  };
+};
